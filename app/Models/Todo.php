41d5dd4a884ec5161f39676f11dd43b0af4b408a
MaputiLeah <116145725+MaputiLeah@users.noreply.github.com>
2023-12-20 01:10:05 +0800

    Update Todo.php

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,10 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+    // Specify the table name for the Todo model
     use HasFactory;
+    // Define the fillable attributes for mass assignment
     protected $table = 'todos';
     protected $fillable = ['tittle', 'description', 'is_completed'];
 
+    /**
+     * Define a relationship between Todo and User models.
+     *
+     * A Todo belongs to a User, indicating that each todo is associated with a specific user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
